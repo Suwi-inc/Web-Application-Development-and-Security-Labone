c8909215d9b24f60705d932f7950534b8ff4879d
suwilanji silwamba <57032120+Suwi-inc@users.noreply.github.com>
2022-04-29 12:53:41 +0300

    added second corrections

--- task.sh
+++ task.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 # создать пустой текстовый файл, Переместить его в новую директорию и добавить в конец файла текущего дату и фразу "Modified"
 # первый параметр - Имя Файла, Второй параметр - Директория
-timestamp() { 
-date 
-}
-mkdir -p /home/$USER/newdirexample
-touch "example1.txt"
-
-fname="example1.txt"
-fdir="/home/$USER/newdirexample"
-
-timestamp >> $fname
+fdir=exampleDirectory
+fname=exampleFile.txt
+touch $fname
+mkdir -p $fdir
+mv $fname $(pwd)/$fdir
+cd $(pwd)/$fdir
+date >> $fname
 echo "Modified"  >> $fname
-
-mv $fname $fdir
 echo "done"
